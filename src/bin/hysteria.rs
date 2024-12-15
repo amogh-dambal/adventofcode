0f82c058864a3d1335077d004fa49a17f242b47d
Amogh <32526674+amogh-dambal@users.noreply.github.com>
2024-12-14 18:41:07 -0800

    feat: advent of code day1 part2

--- src/bin/hysteria.rs
+++ src/bin/hysteria.rs
@@ -1,14 +1,25 @@
 use std::{
     cmp::{max, min},
-    collections::BinaryHeap,
+    collections::{BinaryHeap, HashMap},
     io::stdin,
     iter::zip,
 };
 
 fn main() {
-    part1();
+    //    part1();
+    part2();
 }
 
+fn read_line(line: &str) -> (u32, u32) {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+
+    let left = parts[0].parse::<u32>().unwrap();
+    let right = parts[1].parse::<u32>().unwrap();
+
+    (left, right)
+}
+
+#[allow(unused)]
 fn part1() {
     let mut lefts: Vec<u32> = Vec::new();
     let mut rights: Vec<u32> = Vec::new();
@@ -16,14 +27,7 @@ fn part1() {
     stdin()
         .lines()
         .filter_map(|line| line.ok())
-        .map(|line| {
-            let parts: Vec<&str> = line.split_whitespace().collect();
-
-            let left = parts[0].parse::<u32>().unwrap();
-            let right = parts[1].parse::<u32>().unwrap();
-
-            (left, right)
-        })
+        .map(|line| read_line(&line))
         .for_each(|(left, right)| {
             lefts.push(left);
             rights.push(right);
@@ -36,4 +40,31 @@ fn part1() {
         .map(|(left, right)| max(left, right) - min(left, right))
         .sum();
     println!("{}", total_distance);
+}
+
+#[allow(unused)]
+fn part2() {
+    let mut location_ids: Vec<u32> = Vec::new();
+    let mut similarity_multiplier: HashMap<u32, u32> = HashMap::new();
+
+    stdin()
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| read_line(&line))
+        .for_each(|(left, right)| {
+            location_ids.push(left);
+            similarity_multiplier
+                .entry(right)
+                .and_modify(|val| *val += 1)
+                .or_insert(1);
+        });
+
+    let similarity_score: u32 = location_ids
+        .iter()
+        .map(|location_id| match similarity_multiplier.get(location_id) {
+            Some(val) => location_id * val,
+            None => 0,
+        })
+        .sum();
+    println!("{}", similarity_score);
 }
